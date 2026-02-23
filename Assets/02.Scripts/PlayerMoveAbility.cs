d14da96852a19e4cb3a85cacdcf075a65658f03a
정종혁 <whdgur0068@gmail.com>
2026-02-23 13:32:35 +0900

    feat : 플레이어 움직임 추가

---
+++ Assets/02.Scripts/PlayerMoveAbility.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class PlayerMoveAbility : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 7f;
+    [SerializeField] private float _jumpForce = 2.5f;
+    private const float GRAVITY = 9.8f;
+    [SerializeField] private float _yVelocity = 0f;
+
+    private CharacterController _characterController;
+
+    private void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(v, 0, h);
+        direction.Normalize();
+
+        _yVelocity -= GRAVITY * Time.deltaTime;
+        direction.y = _yVelocity;
+
+        if (Input.GetKey(KeyCode.Space) && _characterController.isGrounded)
+        {
+            _yVelocity = _jumpForce;
+        }
+
+        _characterController.Move(direction * Time.deltaTime * _moveSpeed);
+    }
+}
