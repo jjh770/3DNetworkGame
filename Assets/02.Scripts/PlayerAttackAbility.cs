409bb050a3c90ce343531e6153409a0e2e05e892
정종혁 <whdgur0068@gmail.com>
2026-02-23 15:03:25 +0900

    chore : 공격 쿨타임 추가

--- Assets/02.Scripts/PlayerAttackAbility.cs
+++ Assets/02.Scripts/PlayerAttackAbility.cs
@@ -2,6 +2,9 @@
 
 public class PlayerAttackAbility : MonoBehaviour
 {
+    private float ATTACK_COOLTIME = 0.6f;
+    private float _attackTimer = 0f;
+
     [SerializeField] private Animator _animator;
     [SerializeField] private float _comboResetTime = 1.0f;
     private int _comboStep = 0;
@@ -11,18 +14,24 @@ public class PlayerAttackAbility : MonoBehaviour
     {
         if (_animator == null) return;
 
+        _attackTimer += Time.deltaTime;
+
         if (Time.time - _lastAttackTime > _comboResetTime)
         {
             _comboStep = 0;
         }
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && _attackTimer >= ATTACK_COOLTIME)
         {
+            _attackTimer = 0f;
+
             _animator.SetTrigger($"Attack{Random.Range(1, 4)}");
         }
 
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(1) && _attackTimer >= ATTACK_COOLTIME)
         {
+            _attackTimer = 0f;
+
             _lastAttackTime = Time.time;
             _comboStep++;
 
