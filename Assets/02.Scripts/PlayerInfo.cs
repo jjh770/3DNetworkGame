824700c4431a73e54dcf07241ef755bae8bc79f7
정종혁 <whdgur0068@gmail.com>
2026-02-23 15:00:51 +0900

    feat : 이동, 공격 애니메이션 추가

---
+++ Assets/02.Scripts/PlayerInfo.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerInfo : MonoBehaviour
+{
+    // 1. 중력 적용하기
+    // 2. 스페이스바 누르면 점프하기
+    // 3. 플레이어 이동을 카메라가 바라보는 방향 기준으로 설정하기
+    // 4. Idle/Run 애니메이션을 블렌드로 적용하기
+    // 5. PlayerAttackAbility 스크립트 만들어서
+    // - 마우스 왼쪽 클릭 시마다 Attack1 / Attack2 / Attack3 애니메이션이 아래 옵션에 따라 실행되게 하기
+    // - - 열거형 옵션 1. 순차적
+    // - - 열거형 옵션 2. 랜덤
+}
--- Assets/02.Scripts/PlayerMoveAbility.cs
+++ Assets/02.Scripts/PlayerMoveAbility.cs
@@ -6,6 +6,7 @@ public class PlayerMoveAbility : MonoBehaviour
     [SerializeField] private float _jumpForce = 2.5f;
     private const float GRAVITY = 9.8f;
     [SerializeField] private float _yVelocity = 0f;
+    [SerializeField] private Animator _animator;
 
     private CharacterController _characterController;
 
@@ -16,20 +17,28 @@ public class PlayerMoveAbility : MonoBehaviour
 
     private void Update()
     {
-        float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
 
-        Vector3 direction = new Vector3(v, 0, h);
+        Vector3 direction = new Vector3(h, 0, v);
         direction.Normalize();
 
+        if (_animator != null)
+        {
+            _animator.SetFloat("MoveX", direction.x);
+            _animator.SetFloat("MoveZ", direction.z);
+        }
+
+        Vector3 moveDirection = transform.TransformDirection(direction);
+
         _yVelocity -= GRAVITY * Time.deltaTime;
-        direction.y = _yVelocity;
+        moveDirection.y = _yVelocity;
 
         if (Input.GetKey(KeyCode.Space) && _characterController.isGrounded)
         {
             _yVelocity = _jumpForce;
         }
 
-        _characterController.Move(direction * Time.deltaTime * _moveSpeed);
+        _characterController.Move(moveDirection * Time.deltaTime * _moveSpeed);
     }
 }
