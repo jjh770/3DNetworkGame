3b6071bae5ad35f648075ce98ff791cf20b0f84a
정종혁 <whdgur0068@gmail.com>
2026-02-26 15:50:45 +0900

    feat : BearMonster 추가중

---
+++ Assets/02.Scripts/Monster/BearState.cs
@@ -0,0 +1,9 @@
+﻿public enum BearState
+{
+    Idle,       // 멈춰서 대기
+    Patrol,     // 순찰 중
+    Wait,       // 침입자 감지 (추격 전 경고)
+    Chase,      // 추격 중
+    Attack,     // 공격 중
+    Hit         // 피격 (에어본/경직 등)
+}
